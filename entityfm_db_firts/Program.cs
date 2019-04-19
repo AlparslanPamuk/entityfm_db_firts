d26784307b567eab53d0279deee7c9e875466176
Mustafa Alparslan Pamuk <mustafaalparslanpamuk@gmail.com>
2019-04-19 15:47:09 +0300

    First commit

---
+++ entityfm_db_firts/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entityfm_db_firts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HospitalProjectEntities db = new HospitalProjectEntities();
+            var doctors = db.Tbl_Doctors;
+            foreach (var doc in doctors)
+            {
+                Console.WriteLine(doc.docname);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
